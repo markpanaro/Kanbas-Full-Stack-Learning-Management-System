9b1785a9c7b6ffa989505259c9691909d514990e
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-09-18 13:15:00 -0400

    Finishing a1 part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 // import './App.css';
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div className="App">
+        <h1>Welcome to Web Dev</h1>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,5 +1,8 @@
 export default function Lab2() {
-    return(
+    return (
+      <div>
         <h2>Lab 2</h2>
+      </div>
     );
-}
+  }
+  
