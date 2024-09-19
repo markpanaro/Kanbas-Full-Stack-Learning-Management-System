a3efb12c49843140e43fa23bed55fbebd2cdc66a
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-09-18 21:48:28 -0400

    a1 part 2 start

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 // import './App.css';
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 
 function App() {
-  return (
+  return ( 
     <HashRouter>
       <div className="App">
-        <h1>Welcome to Web Dev</h1>
         <Routes>
           <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
     </HashRouter>
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div>
+            <h1>Mark Panaro</h1>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
