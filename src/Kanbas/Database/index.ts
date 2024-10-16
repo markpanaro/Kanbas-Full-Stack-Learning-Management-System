9ca7a736662eef7a855b6a552e1f6148ad95eb0b
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-10-15 20:50:39 -0400

    a3 3.9

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -1,4 +1,6 @@
 import courses from "./courses-su24.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
-export { courses, modules, assignments };
+import enrollments from "./enrollments.json";
+import users from "./users-fa24.json";
+export { courses, modules, assignments, enrollments, users };
