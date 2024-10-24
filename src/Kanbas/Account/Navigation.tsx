15d3ec8e080d6c9aa717f1f7ce8f1d84e0c6deb3
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-10-24 12:31:11 -0400

    a4 3.4

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+    const { pathname } = useLocation();
     return (
         <div id="wd-account-navigation">
             <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-                <Link id="wd-signin-link" to="/Kanbas/Account/Signin"
-                    className="list-group-item active border border-0">Signin</Link>
-                <Link id="wd-signup-link" to="/Kanbas/Account/Signup"
-                    className="list-group-item text-danger border border-0">Signup</Link>
-                <Link id="wd-profile-link" to="/Kanbas/Account/Profile"
-                    className="list-group-item text-danger border border-0">Profile</Link>
+                {!currentUser && (
+                    <>
+                        <Link id="wd-signin-link" to="/Kanbas/Account/Signin"
+                            className="list-group-item active border border-0">Signin</Link>
+                        <Link id="wd-signup-link" to="/Kanbas/Account/Signup"
+                            className="list-group-item text-danger border border-0">Signup</Link>
+                    </>
+                )}
+                {currentUser && (
+                    <Link id="wd-profile-link" to="/Kanbas/Account/Profile"
+                        className="list-group-item text-danger border border-0">Profile</Link>
+                )}
             </div>
 
         </div>
