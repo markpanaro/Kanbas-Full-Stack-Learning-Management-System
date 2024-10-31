08497ab5bdd33a63d17ce90c652a364e160c59ab
Mark Panaro <72929673+markpanaro@users.noreply.github.com>
2024-10-30 22:48:24 -0400

    a4 2.5.2 fix

--- src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
@@ -1,17 +1,22 @@
-export default function TodoForm({ todo, setTodo, addTodo, updateTodo }: {
-    todo: { id: string; title: string };
-    setTodo: (todo: { id: string; title: string }) => void;
-    addTodo: (todo: { id: string; title: string }) => void;
-    updateTodo: (todo: { id: string; title: string }) => void;
-}) {
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, updateTodo, setTodo } from "./todosReducer";
+
+export default function TodoForm(
+) {
+    const { todo } = useSelector((state: any) => state.todosReducer);
+    const dispatch = useDispatch();
     return (
         <li className="list-group-item">
-            <input value={todo.title}
-                onChange={(e) => setTodo({ ...todo, title: e.target.value })} />
-            <button onClick={() => updateTodo(todo)}
-                id="wd-update-todo-click" className="btn btn-warning m-2"> Update </button>
-            <button onClick={() => addTodo(todo)}
-                id="wd-add-todo-click" className="btn btn-success"> Add </button>
+            <input
+                defaultValue={todo.title}
+                onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value }))} />
+            <button onClick={() => dispatch(updateTodo(todo))}
+                id="wd-update-todo-click"
+                className="btn btn-warning m-2"> Update </button>
+            <button onClick={() => dispatch(addTodo(todo))}
+                id="wd-add-todo-click"
+                className="btn btn-success"> Add </button>
         </li>
     );
 }
--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,14 +1,17 @@
-export default function TodoItem({ todo, deleteTodo, setTodo }: {
-    todo: { id: string; title: string };
-    deleteTodo: (id: string) => void;
-    setTodo: (todo: { id: string; title: string }) => void;
-}) {
+import React from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, setTodo } from "./todosReducer";
+export default function TodoItem({ todo }: any) {
+    const dispatch = useDispatch();
     return (
         <li key={todo.id} className="list-group-item">
             {todo.title} <span className="right-text-margin"></span>
-            <button onClick={() => setTodo(todo)}
-                id="wd-set-todo-click" className="btn btn-primary m-1"> Edit </button>
-            <button onClick={() => deleteTodo(todo.id)}
-                id="wd-delete-todo-click" className="btn btn-danger m-1"> Delete </button>
-        </li>);
+            <button onClick={() => dispatch(setTodo(todo))}
+                id="wd-set-todo-click"
+                className="btn btn-primary m-1"> Edit </button>
+            <button onClick={() => dispatch(deleteTodo(todo.id))}
+                id="wd-delete-todo-click"
+                className="btn btn-danger m-1"> Delete </button>
+        </li>
+    );
 }
